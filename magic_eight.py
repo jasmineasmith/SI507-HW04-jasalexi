7d4c8d7dd17458d1cbc37e19c3ddd3616911f5a7
jasmineasmith <jasminea.smith@yahoo.com>
2018-01-29 20:05:26 -0500

    check

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,8 @@
-ask_user=input("What is your question? (please end in '?')")
+while True:
+	ask_user=input("What is your question? (please end in '?')")
+	if ask_user=="quit"
+		break
+	if ask_user.endswith("?")==False:
+		print("I’m sorry, I can only answer questions.")
+
+

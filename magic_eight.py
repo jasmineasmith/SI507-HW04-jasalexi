1421632434fd4406549896d8a19650fa33944128
biran <biran@0587347157.wireless.umich.net>
2018-01-29 19:51:36 -0500

    add pick an answer

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,10 @@
 ask_user=input("What is your question? (please end in '?')")
+
+eight_ball = ["Signs point to yes","Yes","Reply hazy try again",
+            "Without a doubt","My sources say no","As I see it, yes",
+            "You may rely on it","Concentrate and ask again",
+            "Outlook not so good","It is decidedly so",
+            "Better not tell you now","Very doubtful","Yes definitely",
+            "It is certain","Cannot predict now","Most likely",
+            "Ask again later","My reply is no","Outlook good","Don't count on it"]
+eight_answer = random.choice(eight_ball)
